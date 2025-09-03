362836177508e5b2e606e0c4358b64948faef130
avi garg <gargavi2006@outlook.com>
2025-09-03 23:01:57 +0530

    fixed origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -6,7 +6,7 @@ export const app = express();
 
 app.use(express.json());
 app.use(cors({
-  origin: "http://localhost:5173",
+  origin: "https://grid-powers.vercel.app",
   credentials: true,
 }));
 
